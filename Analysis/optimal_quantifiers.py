c172ff5e9d3a8ecc0000a117e3d3d9d6aed2b981
Wouter Posdijk <wouter@wcw-stud-145-109-46-6.wireless.uva.nl>
2019-02-21 13:55:56 +0100

    Analyze very optimal generated quantfiers

--- Analysis/optimal_quantifiers.py
+++ Analysis/optimal_quantifiers.py
@@ -1,9 +1,14 @@
-import pickle
-
+import numpy as np
 import Parser
 
 quantifiers = list(Parser.load_from_file('../results/GeneratedQuantifiers.json').values())
-with open('../results/generated_meanings.pickle', 'rb') as file:
-    raw_meanings = pickle.load(file)
+costs = np.loadtxt('../results/generated_quantifiers_cost.txt')
+complexities = np.loadtxt('../results/generated_quantifiers_complexity.txt')
 
 universe_size = 20
+
+optimal_quantifiers = []
+
+for (quantifier, cost, complexity) in zip(quantifiers, costs, complexities):
+    if cost < .1 and complexity < .2:
+        print("{0}\ncost: {1}\ncomp: {2}".format(quantifier, cost, complexity))
