b2a351eea0938a300e0265229df911932c0a3dac
Wouter Posdijk <wouter@MacBook-Pro-3.local>
2019-04-29 14:53:59 +0200

    Separate informativeness and complexity measuring + add special complexity measure for languages

--- Code/Languages/ComplexityMeasurer.py
+++ Code/Languages/ComplexityMeasurer.py
@@ -14,3 +14,12 @@ class SumComplexityMeasurer(object):
 
     def __call__(self, language):
         return sum(word.complexity for word in language) / self.max_complexity
+
+
+class SpecialComplexityMeasurer(object):
+
+    def __init__(self, max_words):
+        self.max_complexity = max_words
+
+    def __call__(self, language):
+        return sum(word.special_complexity for word in language) / self.max_complexity
