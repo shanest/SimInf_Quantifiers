26aac964c9a1a60d32b5f0d0ed7a75f49712d1fb
Wouter Posdijk <wouter@wcw-stud-145-109-55-115.wireless.uva.nl>
2019-04-30 14:58:26 +0200

    Add more plotting and modulo natural quantifiers

---
+++ Code/IndividualQuantifiers/Analysis/plot_special_complexity_vs_monotonicity.py
@@ -0,0 +1,24 @@
+import dill
+
+import Measurer
+import Parser
+import analysisutil
+import matplotlib.pyplot as plt
+
+(args, setup, file_util) = analysisutil.init(use_base_dir=True)
+
+complexities = file_util.load_dill('expression_special_complexities.dill')
+monotonicities = file_util.load_dill('monotonicities_max.dill')
+
+fig = plt.figure()
+
+# Plot
+plt.plot(monotonicities,complexities,'o')
+
+# for i in range(len(generated_quantifier_expressions)):
+#     plt.annotate(str(i),(generated_cost[i],generated_complexity[i]))
+
+plt.xlabel('monotonicity')
+plt.ylabel('complexity')
+plt.show()
+file_util.save_figure(fig, 'monotonicity_special_complexity_plot')
