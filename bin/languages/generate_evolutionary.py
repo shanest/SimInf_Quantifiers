784625fb84940fa4191e12b7a2e4656571c05325
Charlie Guo <49220711+qcg1999@users.noreply.github.com>
2020-08-09 23:03:55 -0400

    Responded feedback of pullrequest #6
    Refactored generate_evolutionary.py; moved siminf/analysis.py.

--- bin/languages/generate_evolutionary.py
+++ bin/languages/generate_evolutionary.py
@@ -3,103 +3,71 @@ import itertools
 import math
 import random
 from copy import copy
-
 import pygmo
 from pathos.multiprocessing import ProcessPool
-
-# import Generator
 from siminf import generator 
-# import analysisutil
 from siminf import analysisutil
-
-from siminf import experiment_setups #transferred from analysisutil
-from siminf import fileutil #transferred from analysisutil
-from siminf.fileutil import FileUtil #transferred from analysisutil
-
-# from Languages import LanguageLoader, LanguageGenerator
+from siminf import experiment_setups 
+from siminf import fileutil 
+from siminf.fileutil import FileUtil 
 from siminf.languages import language_loader, language_generator
-
-# from Languages.ComplexityMeasurer import SumComplexityMeasurer
 from siminf.languages.complexity_measurer import SumComplexityMeasurer
-# from Languages.InformativenessMeasurer import SimMaxInformativenessMeasurer
 from siminf.languages.informativeness_measurer import SimMaxInformativenessMeasurer
 
-# analysisutil.add_argument('lang_size', type=int)
-# analysisutil.add_argument('sample_size', type=int)
-# analysisutil.add_argument('generations', type=int)
-# analysisutil.add_argument('-m','--max_mutations', type=int, default=1)
-# (args, setup, file_util) = analysisutil.init()
+def remove(language, expressions=None):
+    language = copy(language)
+    index = random.randint(0,len(language)-1)
+    language.pop(index)
+    return language
 
-# expressions = LanguageLoader.load_all_evaluated_expressions(file_util)
+def add(language, expressions):
+    language = copy(language)
+    while True:
+        new_expression = random.choice(expressions)
+        if new_expression not in language:
+            language.append(new_expression)
+            break
+    return language
 
-# languages_1 = LanguageGenerator.generate_sampled(expressions, args.lang_size, int(args.sample_size/args.lang_size))
+def interchange(language, expressions):
+    return add(remove(language), expressions)
 
-# universe = Generator.generate_simplified_models(args.model_size)
+def mutate(language, expressions):
+    possible_mutations = [interchange]
+    if len(language) < args.lang_size:
+        possible_mutations.append(add)
+    if len(language) > 1:
+        possible_mutations.append(remove)
 
-
-# def remove(language):
-#     language = copy(language)
-#     index = random.randint(0,len(language)-1)
-#     language.pop(index)
-#     return language
-
-# def add(language):
-#     language = copy(language)
-#     while True:
-#         new_expression = random.choice(expressions)
-#         if new_expression not in language:
-#             language.append(new_expression)
-#             break
-#     return language
-
-# def interchange(language):
-#     return add(remove(language))
-
-# def mutate(language):
-#     possible_mutations = [interchange]
-#     if len(language) < args.lang_size:
-#         possible_mutations.append(add)
-#     if len(language) > 1:
-#         possible_mutations.append(remove)
-
-#     mutation = random.choice(possible_mutations)
-#     return mutation(language)
+    mutation = random.choice(possible_mutations)
+    return mutation(language, expressions)
 
 
-# def sample_mutated(languages, amount):
-#     amount -= len(languages)
-#     amount_per_language = int(math.floor(amount / len(languages)))
-#     amount_random = amount % len(languages)
+def sample_mutated(languages, amount, expressions):
+    amount -= len(languages)
+    amount_per_language = int(math.floor(amount / len(languages)))
+    amount_random = amount % len(languages)
 
-#     mutated_languages = []
+    mutated_languages = []
 
-#     for language in languages:
-#         for i in range(amount_per_language):
-#             num_mutations = random.randint(1, args.max_mutations)
-#             mutated_language = language
-#             for j in range(num_mutations):
-#                 mutated_language = mutate(mutated_language)
-#             mutated_languages.append(mutate(language))
+    for language in languages:
+        for i in range(amount_per_language):
+            num_mutations = random.randint(1, args.max_mutations)
+            mutated_language = language
+            for j in range(num_mutations):
+                mutated_language = mutate(mutated_language, expressions)
+            mutated_languages.append(mutate(language, expressions))
 
-#     for i in range(amount_random):
-#         language = random.choice(languages)
-#         mutated_languages.append(mutate(language))
+    for i in range(amount_random):
+        language = random.choice(languages)
+        mutated_languages.append(mutate(language, expressions))
 
-#     mutated_languages.extend(languages)
+    mutated_languages.extend(languages)
 
-#     return mutated_languages
+    return mutated_languages
 
 
-def main(args):
-    
-    print('args.max_quantifier_length={}'.format(args.max_quantifier_length))
-    print('args.model_size={}'.format(args.model_size))
-    print('args.lang_size={}'.format(args.lang_size))
-    print('args.sample_size={}'.format(args.sample_size))
-    print('args.generations={}'.format(args.generations))
-    print('args.processes={}'.format(args.processes))
-    
-    
+def main(args):    
     setup = experiment_setups.parse(args.setup)
     use_base_dir = False
     dirname = fileutil.base_dir(args.dest_dir, setup.name, args.max_quantifier_length, args.model_size) if use_base_dir \
@@ -107,69 +75,13 @@ def main(args):
     file_util = FileUtil(dirname)
     
     expressions = language_loader.load_all_evaluated_expressions(file_util)
-
-    languages_1 = language_generator.generate_sampled(expressions, args.lang_size, int(args.sample_size/args.lang_size))
-
+    languages_0 = language_generator.generate_sampled(expressions, args.lang_size, int(args.sample_size/args.lang_size))
     universe = generator.generate_simplified_models(args.model_size)
-    
-    def remove(language):
-        language = copy(language)
-        index = random.randint(0,len(language)-1)
-        language.pop(index)
-        return language
-    
-    def add(language):
-        language = copy(language)
-        while True:
-            new_expression = random.choice(expressions)
-            if new_expression not in language:
-                language.append(new_expression)
-                break
-        return language
-    
-    def interchange(language):
-        return add(remove(language))
-    
-    def mutate(language):
-        possible_mutations = [interchange]
-        if len(language) < args.lang_size:
-            possible_mutations.append(add)
-        if len(language) > 1:
-            possible_mutations.append(remove)
-    
-        mutation = random.choice(possible_mutations)
-        return mutation(language)
-    
-    
-    def sample_mutated(languages, amount):
-        amount -= len(languages)
-        amount_per_language = int(math.floor(amount / len(languages)))
-        amount_random = amount % len(languages)
-    
-        mutated_languages = []
-    
-        for language in languages:
-            for i in range(amount_per_language):
-                num_mutations = random.randint(1, args.max_mutations)
-                mutated_language = language
-                for j in range(num_mutations):
-                    mutated_language = mutate(mutated_language)
-                mutated_languages.append(mutate(language))
-    
-        for i in range(amount_random):
-            language = random.choice(languages)
-            mutated_languages.append(mutate(language))
-    
-        mutated_languages.extend(languages)
-    
-        return mutated_languages
-    
-    
-    # in main()
+
     measure_complexity = SumComplexityMeasurer(args.lang_size, 1)
     measure_informativeness = SimMaxInformativenessMeasurer(universe)
     pool = ProcessPool(nodes=args.processes)
-    languages = languages_1
+    languages = languages_0    #lanuages will be iteratively updated in subsequent loop
     
     for gen in range(args.generations):
         print('GENERATION {0}'.format(gen))
@@ -184,7 +96,7 @@ def main(args):
         dominating_languages = [languages[i] for i in dominating_indices]
     
         print('mutating')
-        languages = sample_mutated(dominating_languages, args.sample_size)
+        languages = sample_mutated(dominating_languages, args.sample_size, expressions)
     
     language_indices = [[e.index for e in lang] for lang in dominating_languages]
     dominating_complexity = [complexity[i] for i in dominating_indices]
