aa9213eca00bc4e7e71d1a7827c4738f8a103be1
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2021-07-23 15:30:38 -0700

    bootstrapped CIs for correlations

--- bin/languages/analysis/analyze.py
+++ bin/languages/analysis/analyze.py
@@ -19,7 +19,6 @@ def make_plot(data, color_variable, filename):
             pn.scale_color_cmap('cividis'))
     file_util.save_plot(plot, filename, width=6, height=4, dpi=300)
 
-
 """
 make_plot(natural_data, 'naturalness', 'naturalness_with_pareto.png')
 make_plot(random_data, 'monotonicity', 'monotonicity_with_pareto.png')
@@ -47,20 +46,18 @@ def full_analysis(data, predictors, num_bootstrap_samples=5000):
         print(predictor)
         r, _ = scipy.stats.pearsonr(data['optimality'], data[predictor])
         print(r)
-        rs = [r]
-        for _ in range(num_bootstrap_samples):
-            bootstrap_sample = data.sample(n=len(data), replace=True)
-            r, _ = scipy.stats.pearsonr(bootstrap_sample['optimality'], bootstrap_sample[predictor])
-            rs.append(r)
-        print(scipy.stats.scoreatpercentile(rs, (2.5, 97.5)))
-
-    """
-    right_hands = predictors + ['*'.join(predictors)]
-    for rhs in right_hands:
-        fit_stat_model('optimality ~ {}'.format(rhs), data)
-    """
+        for bootstrap_sample_percent in np.geomspace(0.01, 1.0, num=5):
+            rs = []
+            for _ in range(num_bootstrap_samples):
+                bootstrap_sample = data.sample(n=int(bootstrap_sample_percent*len(data)), replace=True)
+                r, _ = scipy.stats.pearsonr(bootstrap_sample['optimality'], bootstrap_sample[predictor])
+                rs.append(r)
+            print(scipy.stats.scoreatpercentile(rs, (2.5, 97.5)))
 
 
 predictors = ['monotonicity', 'conservativity']
+print('random data')
 full_analysis(random_data, predictors)
+print()
+print('natural data')
 full_analysis(natural_data, predictors + ['naturalness'])
